c3917c3073cf9664e1a2444077ff60c9ed93f542
donamecarlos <cs8899112@gmail.com>
2026-06-12 02:05:25 -0600

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://cs8899112_db:foreingsinix@ac-mygfsoy-shard-00-00.f1aoovq.mongodb.net:27017,ac-mygfsoy-shard-00-01.f1aoovq.mongodb.net:27017,ac-mygfsoy-shard-00-02.f1aoovq.mongodb.net:27017/?ssl=true&replicaSet=atlas-btfgum-shard-0&authSource=admin&appName=Cluster0")
-.then(() => console.log("estado de la conexion con atlas: Conectado"))
-.catch(err => console.log(err));
-
-module.exports = mongoose;
